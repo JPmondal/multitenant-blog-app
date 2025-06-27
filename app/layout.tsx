124a3f14038abc02162b481cd4c16ebf790459a7
JP Mondal <jpmondal2003@gmail.com>
2025-06-27 12:17:33 +0600

    check commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider,SignedIn,SignedOut,SignIn } from "@clerk/nextjs";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,14 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+       <ClerkProvider>
+        <SignedOut>
+          <div className="flex justify-center items-center min-h-screen min-w-screen">
+            <SignIn routing="hash" />
+          </div>
+          </SignedOut>
+         <SignedIn>{children}</SignedIn>
+       </ClerkProvider>
       </body>
     </html>
   );
