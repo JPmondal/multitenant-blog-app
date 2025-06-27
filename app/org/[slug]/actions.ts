e026632be0376a0e98748157b6bd68c2ca41483c
JP Mondal <jpmondal2003@gmail.com>
2025-06-27 22:06:11 +0600

    successfull postgress blog entry with drizzle

---
+++ app/org/[slug]/actions.ts
@@ -0,0 +1,13 @@
+"use server";
+
+import { CreateBlogType, blogTable } from "@/db/schema";
+import { db } from "@/db";
+
+export const createBlog = async (payload: CreateBlogType) => {
+    
+  const [result] = await db.insert(blogTable).values(payload).returning({
+    id: blogTable.id,
+  });
+
+  return result.id;
+};
--- app/org/[slug]/page.tsx
+++ app/org/[slug]/page.tsx
@@ -1,27 +1,51 @@
 "use client";
 
-import Nav from "@/app/components/nav"
-import { Button } from "@/components/ui/button"
-import { Textarea } from "@/components/ui/textarea"
+import Nav from "@/app/components/nav";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
 import { useState } from "react";
 import { Input } from "@/components/ui/input";
+import { createBlog } from "./actions";
+import { useOrganization } from "@clerk/nextjs";
+
+export default function OrgLandingPage() {
+  const [blogContent, setBlogContent] = useState("");
+  const [blogTitle, setBlogTitle] = useState("");
+  const selectedOrg = useOrganization();
 
 
-export default function OrgLandingPage(){
+  const handleCreateBlog = async () => {
 
-    const [blogContent, setBlogContent] = useState('')
-    const [blogTitle, setBlogTitle] = useState('')
+    if (!selectedOrg.organization) {
+      return;
+    }
 
-    return (
-        <main>
-            <Nav />
-            <div className="p-10">
-                <Input onChange={(e)=>setBlogTitle(e.target.value)} value={blogTitle} placeholder="Enter Blog title"/>
+    await createBlog({
+      body: blogContent.trim(),
+      orgId: selectedOrg.organization?.id,
+      title: blogTitle,
+    });
+  };
 
-                <Textarea className="mt-2" placeholder="Enter you blog content" onChange={(e)=>setBlogContent(e.target.value)} value={blogContent}/>
-                    
-                <Button className="mt-2">Create a Blog</Button>
-            </div>
-        </main>
-    )
+  return (
+    <main>
+      <Nav />
+      <div className="p-10">
+        <Input
+          onChange={(e) => setBlogTitle(e.target.value)}
+          value={blogTitle}
+          placeholder="Enter Blog title"
+        />
+
+        <Textarea
+          className="mt-2"
+          placeholder="Enter you blog content"
+          onChange={(e) => setBlogContent(e.target.value)}
+          value={blogContent}
+        />
+
+        <Button onClick={()=> handleCreateBlog()} className="mt-2">Create a Blog</Button>
+      </div>
+    </main>
+  );
 }
